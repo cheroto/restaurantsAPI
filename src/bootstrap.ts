eb3244a2106883fc42ca53d8a48f807bf1aeee67
Pedro <pedro.cheroto@gmail.com>
2020-05-12 12:50:52 -0400

    Enabled CORS for front-end.

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -14,6 +14,12 @@ server.setConfig((app) => {
     extended: true
   }));
   app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', "*");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 });
 
 let serverInstance = server.build();
